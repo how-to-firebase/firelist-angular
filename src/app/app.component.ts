dba2f17313db12629cd244c30651910a3b21566e
Juarez Filho <juarezpaf@gmail.com>
2018-05-01 23:19:41 +0100

    feat(ui): register svg icons for sign in buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,34 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { Observable } from 'rxjs/Observable';
-
-export interface Link {
-  name: string;
-  url: string;
-}
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  @ViewChild('newLinkName') newLinkName: ElementRef;
-  @ViewChild('newLink') newLink: ElementRef;
-
   title = 'Firelist ⚡';
-  links$: Observable<Link[]>;
-  linksRef: AngularFireList<Link>;
 
-  constructor(db: AngularFireDatabase) {
-    this.linksRef = db.list<Link>('links');
-    this.links$ = this.linksRef.snapshotChanges().map(changes => {
-      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-    });
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    /** ICONS **/
+    iconRegistry.addSvgIcon(
+      'google',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/auth/google.svg'));
+
+    iconRegistry.addSvgIcon(
+      'facebook',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/auth/facebook.svg'));
+
+    iconRegistry.addSvgIcon(
+      'twitter',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/auth/twitter.svg'));
+
+    iconRegistry.addSvgIcon(
+      'github',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/auth/github.svg'));
   }
 
-  async addLink(newName: string, url: string) {
-    const linkNameInput = this.newLinkName.nativeElement;
-    const linkUrlInput = this.newLink.nativeElement;
-
-    if ( linkNameInput.value && linkUrlInput.value ) {
-      const newLink: Link = { name: newName, url };
-      await this.linksRef.push(newLink);
-
-      linkNameInput.value = '';
-      linkUrlInput.value = '';
-    }
-  }
-
-  deleteLink(key: string) {
-    this.linksRef.remove(key);
-  }
 }
