4a0aa42441fecf44ac37953f41ee0971546521ed
Juarez Filho <juarezpaf@gmail.com>
2018-05-04 05:38:32 +0100

    feat(notes): adding your first note (#21)
    * feat(notes): NotesModule + routing
    
    * feat(notes): add new note

---
+++ src/app/notes/components/notes-list/notes-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-list',
+  templateUrl: './notes-list.component.html'
+})
+export class NotesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/notes/models/note.model.ts
@@ -0,0 +1,11 @@
+export class Note {
+  id?: string;
+  title: string;
+  description?: string;
+  dueDate?: Date;
+  location?: string;
+  createdAt: any;
+  createdBy?: any;
+  photoURL: string;
+  owner: string;
+}
