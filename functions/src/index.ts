c9d170d1eb3b994d24b03fa16b48111f4bd143d7
Juarez Filho <juarezpaf@gmail.com>
2018-11-24 13:41:32 +0100

    feat(collaboration): send email via Cloud Functions after sharing a note

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as nodemailer from 'nodemailer';
+import * as nodemoji from 'node-emoji';
 
 const serviceAccount = require('../firebase-adminsdk-credentials.json');
 
@@ -69,3 +71,54 @@ export const deleteNoteAndTodos = functions.region('europe-west1').firestore
       console.log('Todos are gone!');
     });
   });
+
+
+// Configure the email transport using the default SMTP transport and a GMail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// 3. firebase functions:config:set gmail.email="myusername@gmail.com" gmail.password="secretpassword"
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+const APP_NAME = 'Firelist PWA';
+// Sends an email to the given collaborator.
+const sendEmail = async (mailOptions) => {
+  await mailTransport.sendMail(mailOptions);
+  console.log('New email sent to:', mailOptions.to);
+  return null;
+}
+
+// [START shareNoteViaEmail]
+export const shareNoteViaEmail = functions.firestore
+  .document('notes/{noteId}')
+  .onUpdate((change, context) => {
+    const previousNoteValue = change.before.data();
+    const newNoteValue = change.after.data();
+
+    if (newNoteValue.collaborators.length > previousNoteValue.collaborators.length) {
+      const randomEmoji = nodemoji.random();
+      const collaboratorEmail = newNoteValue.collaborators[newNoteValue.collaborators.length - 1];
+
+      const mailOptions = {
+        from: `${APP_NAME} <firelist.pwa@gmail.com>`,
+        to: collaboratorEmail,
+        subject: `${randomEmoji.emoji} You've received an invitation to collaborate`,
+        text: `Hey there!\n\nGreat news! You just got invited to collaborate. Access https://firelist-angular-dev.firebaseapp.com/note/${context.params.noteId} and have fun!\n\n-----------------\n${newNoteValue.title}\n${newNoteValue.description ? `${newNoteValue.description}\n` : ''}${newNoteValue.geolocation ? newNoteValue.geolocation.formatted_address : ''}`
+      };
+
+      return sendEmail(mailOptions);
+    } else {
+      return null;
+    }
+  });
+// [END shareNoteViaEmail]
